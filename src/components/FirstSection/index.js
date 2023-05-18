147ae4ad03c82ec3b9fd3b43ca05143bfba7b120
Cristian Jigau <Cristian.Jigau@endava.com>
2023-05-18 18:14:56 +0300

    Adds global font family

--- src/components/FirstSection/index.js
+++ src/components/FirstSection/index.js
@@ -7,7 +7,6 @@ const FirstSection = () => {
     <>
       <div className="our_image" />
       <div className="first_section">
-        <div className="upper_section"></div>
         <h1>Cristian și Ana</h1>
         <h5>20 | octombrie | 2023</h5>
         <Countdown />
